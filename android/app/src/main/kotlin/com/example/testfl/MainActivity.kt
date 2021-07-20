152cf82222ab15f973302fca51e4c548250b678f
Paweł Żydziak <pawel.zydziak@gmail.com>
2021-07-20 19:41:24 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/testfl/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.testfl
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Welcome to Flutter'),
+        ),
+        body: WebView(
+          initialUrl: "https://google.com",
+          javascriptMode: JavascriptMode.unrestricted,
+          )
+        ),
+      );
+  }
+}
