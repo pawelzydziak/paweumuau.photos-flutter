2420fe6977fa58ef2f43ef686b35ffc5d89bbb4f
Paweł Żydziak <pawel.zydziak@gmail.com>
2021-07-20 20:17:19 +0200

    Back button

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,63 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'dart:async';
 
-void main() => runApp(MyApp());
+// TODO ikona, powiadomienia
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: WebView(
-          initialUrl: "https://google.com",
+      title: 'Paweumuau Photos',
+      home: MyHomePage(title: 'Home', url: 'https://paweumuau.photos/'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title, required this.url});
+
+  final String title;
+  final String url;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late WebViewController _controller;
+
+  final Completer<WebViewController> _controllerCompleter =
+      Completer<WebViewController>();
+
+  Future<bool> _onWillPop(BuildContext context) async {
+    if (await _controller.canGoBack()) {
+      _controller.goBack();
+      return Future.value(false);
+    } else {
+      return Future.value(true);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () => _onWillPop(context),
+      child: Scaffold(
+        body: SafeArea(
+            child: WebView(
+          key: UniqueKey(),
+          onWebViewCreated: (WebViewController webViewController) {
+            _controllerCompleter.future.then((value) => _controller = value);
+            _controllerCompleter.complete(webViewController);
+          },
           javascriptMode: JavascriptMode.unrestricted,
-          )
-        ),
-      );
+          initialUrl: widget.url,
+        )),
+      ),
+    );
   }
 }
